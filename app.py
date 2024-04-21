6a7e886f4f53cf8173197e6b89a9a58930116730
Your Name <leonliu1008@gmail.com>
2024-04-21 23:44:57 +0800

    CRUD-PY done

--- app.py
+++ app.py
@@ -28,10 +28,34 @@ def insert():
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-        print(name)
+        print("新增來自前端的name:",name)
         cur = mysql.connect.cursor()
         cur.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
         mysql.connect.commit()
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connect.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s", (id_data,))
+    print("刪除學生_ID:",id_data)
+    mysql.connect.commit()
+    return redirect(url_for('Index'))
+
+@app.route('/update', methods = ['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connect.cursor()
+        cur.execute("""
+        UPDATE students SET name=%s, email=%s, phone=%s WHERE id=%s
+        """,(name, email, phone, id_data))
+        flash("Data Updated Successfully")
         return redirect(url_for('Index'))
 
 if __name__ == "__main__":
